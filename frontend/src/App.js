2137c89ca680efe026579efccb989de8f827ffca
NT Blok <ntblok.phd@gmail.com>
2025-09-05 19:55:45 -0700

    improve dockerization

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Financial Anomaly Detection</h1>
+        <p>Welcome to the Financial Anomaly Detection Dashboard</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
